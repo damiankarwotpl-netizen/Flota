4227bea688e206651e1ee8406b58c7123f78d64f
damiankarwotpl-netizen <damiankarwot.pl@gmail.com>
2026-03-16 22:13:45 +0100

    Update google_api.py

--- master_app/master_sync/google_api.py
+++ master_app/master_sync/google_api.py
@@ -1,1 +1,19 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from .config import SERVICE_ACCOUNT_FILE, GOOGLE_SHEET_ID
 
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+def connect():
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        SERVICE_ACCOUNT_FILE,
+        scope
+    )
+
+    client = gspread.authorize(creds)
+
+    return client.open_by_key(GOOGLE_SHEET_ID)
