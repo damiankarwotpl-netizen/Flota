6bc97d1ff31be3a6d5df5c8cbb2ece072049b4cc
damiankarwotpl-netizen <damiankarwot.pl@gmail.com>
2026-03-20 11:24:31 +0100

    Restore Gradle launchers for Android build

--- android-native/app-admin/src/main/java/com/future/ultimate/admin/MainActivity.kt
+++ android-native/app-admin/src/main/java/com/future/ultimate/admin/MainActivity.kt
@@ -3,11 +3,13 @@ package com.future.ultimate.admin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.future.ultimate.admin.ui.AdminRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent { AdminRoot() }
     }
 }
