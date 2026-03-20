8663348efe253b878df644c49e358be45a1ae16f
damiankarwotpl-netizen <damiankarwot.pl@gmail.com>
2026-03-20 21:40:03 +0100

    Fix HomeScreen compile error after UI refresh

--- android-native/core-common/build.gradle.kts
+++ android-native/core-common/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -13,6 +14,7 @@ android {
 
     buildFeatures {
         buildConfig = false
+        compose = true
     }
 
     compileOptions {
@@ -26,6 +28,9 @@ android {
 }
 
 dependencies {
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material3:material3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 }
